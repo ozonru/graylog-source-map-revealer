7e9e602dbe6ca105615d7bbfd99d6b1e63b76963
Ivan Kopeykin <ikopeykin@ozon.ru>
2020-04-28 01:04:14 +0300

    improve plugin

--- src/web/components/RevealedSource.jsx
+++ src/web/components/RevealedSource.jsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 
 const regex = /\s*at (?:[^\s]* )?\(?(.*\.js):(\d+):(\d+)\)?/;
@@ -7,8 +6,82 @@ export function isEnabled({value}) {
 	return regex.test(value);
 }
 
-export class RevealedSource extends React.Component {
+class Loader extends React.Component {
+	constructor(props, ctx) {
+		super(props, ctx);
+
+		this.state = {
+			value: 0
+		};
+
+		this.interval = setTimeout(() =>  {
+			this.setState((state) => ({
+				value: (state.value + 1) % 3
+			}));
+		}, 15);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.interval);
+	}
+
 	render() {
-		return `REVEALED ` + JSON.stringify(this.props);
+		return <pre>Loading {Array.from({length: this.state.value + 1}).join('.')}</pre>
+	}
+}
+
+export class RevealedSource extends React.Component {
+	constructor(props, ctx) {
+		super(props, ctx);
+
+		this.state = {
+			loading: true,
+			revealed: null
+		};
+	}
+
+	componentDidMount() {
+		this.abortController = new AbortController();
+
+		fetch('http://localhost:3000/map', {
+			method: 'POST',
+			body: this.props.value,
+			signal: controller.signal
+		})
+		.then(res => res.json())
+		.then(revealed => this.setState({
+			loading: false,
+			revealed
+		}))
+		.catch(err => {
+			console.error(err);
+			this.setState({loading: false})
+		})
+		.then(() => {
+			this.abortController = null;
+		})
+	}
+
+	componentWillUnmount() {
+		if (this.abortController) {
+			this.abortController.abort();
+		}
+	}
+
+	render() {
+		const {
+			loading,
+			revealed
+		} = this.state;
+
+		if (loading) {
+			return <Loader />;
+		}
+
+		if (!loading && revealed === null) {
+			return <pre>INTERNAL CONSUMER ERROR</pre>;
+		}
+
+		return <pre>{this.state.revealed}</pre>;
 	}
 }
